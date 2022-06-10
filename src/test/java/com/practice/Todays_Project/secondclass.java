82b8d7e58ce2d5edc6ae202453fef7437c48005c
Tahmina <tkhanam11@gmail.com>
2022-06-10 18:23:36 -0500

    codeadded

---
+++ src/test/java/com/practice/Todays_Project/secondclass.java
@@ -0,0 +1,14 @@
+package com.practice.Todays_Project;
+
+public class secondclass {
+
+	public static void main(String[] args) {
+		String X ="Asma";
+		String y="Kaniz";
+		String a ="Fahmi";
+		String b ="Mina";
+		int c=2;
+
+	}
+
+}
